f5a6c58d55188bb9830f6a37be9386e93de5e3f7
DanielV <daniel@vikstrom.name>
2024-10-04 14:35:53 +0200

    Can read next-pickup and has a fake sensor to trigger updates

---
+++ custom_components/avfallsapp/__init__.py
@@ -0,0 +1,227 @@
+"""Main package for coordinator."""
+
+from __future__ import annotations
+
+# import datetime as dt
+from datetime import timedelta
+import async_timeout
+import logging
+
+import requests
+# from aiohttp import ClientSession
+
+from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
+
+from homeassistant.const import STATE_UNAVAILABLE, STATE_UNKNOWN, Platform
+from homeassistant.const import CONF_API_KEY, CONF_NAME, CONF_URL
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
+from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+
+# from homeassistant.helpers.event import async_track_state_change_event
+# from homeassistant.util import dt as dt_util
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+# PLATFORMS = []
+PLATFORMS = [Platform.SENSOR]
+
+
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    """Set up this integration using UI."""
+    config_entry.async_on_unload(config_entry.add_update_listener(async_reload_entry))
+
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = {}
+
+    if config_entry.entry_id not in hass.data[DOMAIN]:
+        coordinator = AvfallsappCoordinator(hass, config_entry)
+        hass.data[DOMAIN][config_entry.entry_id] = coordinator
+
+    # if config_entry is not None:
+    #     if config_entry.source == SOURCE_IMPORT:
+    #         hass.async_create_task(
+    #             hass.config_entries.async_remove(config_entry.entry_id)
+    #         )
+    #         return False
+
+    await hass.data[DOMAIN][config_entry.entry_id].async_config_entry_first_refresh()
+
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unloading a config_flow entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
+
+
+async def async_reload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
+    """Reload the config entry."""
+    await async_unload_entry(hass, config_entry)
+    await async_setup_entry(hass, config_entry)
+
+
+# class PoolLabCoordinator(DataUpdateCoordinator):
+#     def __init__(self, hass, api: PoolLabApi) -> None:
+#         """Initialize my coordinator."""
+#         super().__init__(
+#             hass,
+#             _LOGGER,
+#             name="PoolLab API",
+#             update_interval=timedelta(seconds=30),
+#             update_method=self._async_update_data,
+#         )
+#         self.api = api
+
+#     async def _async_update_data(self):
+#         """Fetch data from API endpoint."""
+#         try:
+#             async with async_timeout.timeout(10):
+#                 return await self.api.request()
+#         # except ApiAuthError as err:
+#         # except GraphQLErroras as err:
+#         except TransportQueryError as err:
+#             # Raising ConfigEntryAuthFailed will cancel future updates
+#             # and start a config flow with SOURCE_REAUTH (async_step_reauth)
+#             raise ConfigEntryAuthFailed from err
+#         except Exception as err:
+#             raise UpdateFailed(f"Unknown error communicating with API: {err}") from err
+
+
+class AvfallsappCoordinator(DataUpdateCoordinator):
+    """API base class."""
+
+    def __init__(self, hass: HomeAssistant, config_entry: ConfigEntry) -> None:
+        """Initialize my coordinator."""
+        super().__init__(
+            hass,
+            _LOGGER,
+            name=config_entry.data.get(CONF_NAME),
+            # update_interval=timedelta(hours=1),
+            update_interval=timedelta(seconds=20),
+            update_method=self._async_update_data,
+            # update_method=self._update_data,
+            always_update=True,
+        )
+        self._hass = hass
+        # self._config = config_entry
+        # await self._async_update_data()
+
+    # @property
+    # def name(self) -> str:
+    #     """Name of instance."""
+    #     return self._config.data["name"]
+
+    def get_device_info(self) -> DeviceInfo:
+        """Get device info to group entities."""
+        return DeviceInfo(
+            # identifiers={(DOMAIN, self._config.data[CONF_TYPE])},
+            name=self.name,
+            manufacturer="Avfallsapp.se",
+            entry_type=DeviceEntryType.SERVICE,
+        )
+
+    def get_next_pickup(self):
+        url = (
+            self.config_entry.data.get(CONF_URL) + "/wp-json/nova/v1/next-pickup/list?"
+        )
+        if "http" not in url:
+            url = "https://" + url
+        headers = {
+            "X-App-Identifier": self.config_entry.data.get(CONF_API_KEY),
+        }
+        return requests.get(url, headers=headers, timeout=10)
+
+    # def _update_data(self):
+    async def _async_update_data(self):
+        """Update call function."""
+        _LOGGER.debug("Updating service")
+
+        # https://community.home-assistant.io/t/garbage-sensor-from-api-json-help/155379/4
+        # https://soderkoping.avfallsapp.se/wp-json/nova/v1/
+        # https://soderkoping.avfallsapp.se/wp-json/nova/v1/recycle-stations?
+        # https://soderkoping.avfallsapp.se/wp-json/nova/v1/next-pickup/list?
+
+        # https://developers.home-assistant.io/docs/api/rest/
+
+        # url = self.config_entry.data.get(CONF_URL) + "/wp-json/nova/v1/"
+        url = (
+            self.config_entry.data.get(CONF_URL) + "/wp-json/nova/v1/next-pickup/list?"
+        )
+        if "http" not in url:
+            url = "https://" + url
+        headers = {
+            "X-App-Identifier": self.config_entry.data.get(CONF_API_KEY),
+        }
+
+        # websession = async_get_clientsession(self._hass, verify_ssl=False)
+        # api = await get_api_object(
+        #     self.config_entry.data.get(CONF_URL), "", "", websession
+        # )
+        # reponse = api.get("wp-json/nova/v1/")
+
+        # client = ClientSession(
+        #     base_url=self.config_entry.data.get(CONF_URL),
+        #     headers={
+        #         "X-App-Identifier": "00bd24a377fd8d20",
+        #     },
+        # )
+        # response = client.get("wp-json/nova/v1/")
+
+        try:
+            # Note: asyncio.TimeoutError and aiohttp.ClientError are already
+            # handled by the data update coordinator.
+            async with async_timeout.timeout(10):
+                # Grab active context variables to limit data required to be fetched from API
+                # Note: using context is not required if there is no need or ability to limit
+                # data retrieved from API.
+                response = await self._hass.async_add_executor_job(self.get_next_pickup)
+                # response = requests.get(url, headers=headers, timeout=10)
+                response.raise_for_status()
+                data = response.json()
+                _LOGGER.debug(response.text)
+                _LOGGER.debug(data)
+                # listening_idx = set(self.async_contexts())
+                # return await self.my_api.fetch_data(listening_idx)
+        # except ApiAuthError as err:
+        #     # Raising ConfigEntryAuthFailed will cancel future updates
+        #     # and start a config flow with SOURCE_REAUTH (async_step_reauth)
+        #     raise ConfigEntryAuthFailed from err
+        # except ApiError as err:
+        #     raise UpdateFailed(f"Error communicating with API: {err}")
+        except Exception as err:
+            pass
+            # raise UpdateFailed(f"Unknown error communicating with API: {err}") from err
+
+        # response = requests.get(url, headers=headers, timeout=5)
+        # response.raise_for_status()
+        # data = response.json()
+        # _LOGGER.debug(response.text)
+        # _LOGGER.debug(data)
+
+        pass
+
+
+# class AvfallsappEntity(Entity):
+#     """Base class for Avfallsapp entities."""
+
+#     def __init__(
+#         self,
+#         coordinator: AvfallsappCoordinator,
+#     ) -> None:
+#         """Initialize entity."""
+#         # Input configs
+#         self._coordinator = coordinator
+#         self._attr_device_info = coordinator.get_device_info()
+
+#     @property
+#     def should_poll(self):
+#         """No need to poll. Coordinator notifies entity of updates."""
+#         return False
---
+++ custom_components/avfallsapp/sensor.py
@@ -0,0 +1,137 @@
+"""Sensor integration for Avfallsapp"""
+
+from __future__ import annotations
+import logging
+
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.sensor.const import SensorStateClass
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from . import DOMAIN, AvfallsappCoordinator
+# from .lib import avfallsapp
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities
+):
+    """Setup sensor platform for the ui"""
+    api_coordinator = hass.data[DOMAIN][config_entry.entry_id]
+    # for a in api_coordinator.data.Accounts:
+    #     params = list(set([m.parameter for m in a.Measurements]))
+    #     _LOGGER.debug(
+    #         "Account: id: %s, Name %s, parameters %s", a.id, a.full_name, params
+    #     )
+    #     sorted_meas = sorted(a.Measurements, key=lambda x: x.timestamp, reverse=True)
+    #     param_added = []
+    #     for m in sorted_meas:
+    #         if m.parameter not in param_added:
+    #             _LOGGER.debug(
+    #                 "Meas: id %s, date %s, parameter %s, scneario %s, value %s, unit %s",
+    #                 m.id,
+    #                 m.timestamp,
+    #                 m.parameter,
+    #                 m.scenario,
+    #                 m.value,
+    #                 m.unit,
+    #             )
+    #             param_added.append(m.parameter)
+    #             async_add_entities([MeasurementSensor(api_coordinator, a, m)])
+
+    async_add_entities([MeasurementSensor(api_coordinator, config_entry.entry_id)])
+    return True
+
+
+class MeasurementSensor(CoordinatorEntity, SensorEntity):
+    """Base class for avfallsapp sensor"""
+
+    def __init__(
+        self,
+        coordinator: AvfallsappCoordinator,
+        # account: avfallsapp.Account,
+        # meas: avfallsapp.Measurement,
+        idx,
+    ) -> None:
+        super().__init__(coordinator, idx)
+        # self._account = account
+        # self._latest_measurement = meas
+        self._latest_measurement = None
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        try:
+            self._latest_measurement = self.coordinator.data.get_measurement(
+                self._account.id, self._latest_measurement.parameter
+            )
+            self.async_write_ha_state()
+        except StopIteration:
+            pass
+            # _LOGGER.error(
+            #     "Could not find a measurement matching id:%s and parameter:%s",
+            #     self._account.id,
+            #     self._latest_measurement.parameter,
+            # )
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        # self._attr_native_value = self.coordinator.data[self.idx]["state"]
+        self._attr_native_value = 4
+        self.async_write_ha_state()
+
+    # @property
+    # def _attr_unique_id(self) -> str:
+    #     return "%s_account%s_%s" % (
+    #         self.coordinator.data.id,
+    #         # self._account.id,
+    #         # self._latest_measurement.parameter.replace(" ", "_")
+    #         "".replace("-", "_").lower(),
+    #     )
+
+    # @property
+    # def _attr_name(self) -> str:
+    #     return "%s %s" % (self._account.full_name, self._latest_measurement.parameter)
+
+    # @property
+    # def _attr_device_info(self) -> DeviceInfo:
+    #     """Return a inique set of attributes for each vehicle."""
+    #     return DeviceInfo(
+    #         identifiers={(DOMAIN, self._account.id)},
+    #         name=self._account.full_name,
+    #         model="%sm3" % self._account.volume,
+    #         manufacturer="Avfallsapp",
+    #     )
+
+    # @property
+    # def _attr_native_value(self):
+    #     return self._latest_measurement.value
+
+    # @property
+    # def _attr_native_unit_of_measurement(self) -> str:
+    #     return self._latest_measurement.unit.split(" ")[0]
+
+    # @property
+    # def _attr_state_class(self) -> SensorStateClass:
+    #     return SensorStateClass.MEASUREMENT
+
+    # @property
+    # def _attr_icon(self) -> str:
+    #     return "mdi:water-percent"
+
+    # @property
+    # def _attr_extra_state_attributes(self):
+    #     """Provide attributes for the entity"""
+    #     return {
+    #         "measured_at": self._latest_measurement.timestamp,
+    #         "measure": self._latest_measurement.id,
+    #         "ideal_low": self._latest_measurement.ideal_low,
+    #         "ideal_high": self._latest_measurement.ideal_high,
+    #         "device_serial": self._latest_measurement.device_serial,
+    #         "operator_name": self._latest_measurement.operator_name,
+    #         "comment": self._latest_measurement.comment,
+    #     }
